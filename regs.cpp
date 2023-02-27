bc0e3a06d49c57c5b9e73eee1a0daa96604a147b
Evgeny Sobolev <hwswdevelop@gmail.com>
2023-02-27 20:37:46 +0500

    hi3516ev200 pll initialization.
    So, I add some interesting things.
    How to change register address but don't change description.
    This is needed if you are using array of registers, or array of registers groups.

--- regs.cpp
+++ regs.cpp
@@ -22,16 +22,6 @@ void pllInit() {
 		Step 3. Wait 0.1ms and observe the PLL LOCK indicator register PERI_CRG_PLL122 [10:0].
 		Step 4. If the PLL LOCK register is in the LOCK state, the PLL configuration is complete, otherwise go back to step 3 until the PLL LOCK register is in the LOCK state.
 	*/
-	
-	/*
-		Compiled with: gcc version 12.2.1 20221205 (15:12.2.rel1-1)
-		Compile flags:
-		-fPIC -std=c++17\
-		-march=armv8-a -mgeneral-regs-only\
-		-msoft-float -nostdlib -marm -fno-exceptions\ 
-		-Wall -fno-stack-protector \
-		-O1
-	*/
 
 	// Switch all clocks to 24MHz clock
 	Register::Write< SocClkSel,
@@ -50,34 +40,34 @@ void pllInit() {
 /*
 ;Prepare, load Register(RegisterBase) addresss
 14000a88:       e3a03000        mov     r3, #0				; 0x0000
-14000a8c:       e3413201        movt    r3, #4609       		; 0x1201, i.e. R3 <= 0x12010000
+14000a8c:       e3413201        movt    r3, #4609       	; 0x1201, i.e. R3 <= 0x12010000
 
 14000a90:       e3a02000        mov     r2, #0				; R2 <= 0, i.e all clocks is 24MHz
-14000a94:       e5832080        str     r2, [r3, #128]  		; [0x12010000] <= 0
-14000a98:       f57ff04e        dsb     st				; Write bariler instruction
+14000a94:       e5832080        str     r2, [r3, #128]  	; [0x12010000] <= 0
+14000a98:       f57ff04e        dsb     st					; Write bariler instruction
 */	
 
 	/* Setup PLLA - bypass mode */
 	PllConfig1::Bypass::set(PllConfig1::Bypass::Type::Bypass);
 /*
-14000a9c:       f57ff04d        dsb     ld				; Read barier
+14000a9c:       f57ff04d        dsb     ld						; Read barier
 14000aa0:       e5932004        ldr     r2, [r3, #4]			; Load register value R2 <= [0x12010004]
-14000aa4:       e3822301        orr     r2, r2, #67108864       	; R2 |= 0x4000000, i.e. set Bypass
+14000aa4:       e3822301        orr     r2, r2, #67108864       ; R2 |= 0x4000000, i.e. set Bypass
 14000aa8:       e5832004        str     r2, [r3, #4]			; [0x12010004] <= R2, i.e. Write register
-14000aac:       f57ff04e        dsb     st				; Write barier
+14000aac:       f57ff04e        dsb     st						; Write barier
 */
 
 	/* Setup PLLV - bypass mode */
 	PllConfig7::Bypass::set(PllConfig7::Bypass::Type::Bypass);
 /*
-14000ab0:       f57ff04d        dsb     ld				; Read barier
+14000ab0:       f57ff04d        dsb     ld						; Read barier
 14000ab4:       e593201c        ldr     r2, [r3, #28]			; Load register value R2 <= [0x1201001C]
-14000ab8:       e3822301        orr     r2, r2, #67108864       	; R2 |= 0x4000000, i.e. set Bypass
+14000ab8:       e3822301        orr     r2, r2, #67108864       ; R2 |= 0x4000000, i.e. set Bypass
 14000abc:       e583201c        str     r2, [r3, #28]			; [0x1201001C] <= R2, i.e. Write register
-14000ac0:       f57ff04e        dsb     st				; Write barier
+14000ac0:       f57ff04e        dsb     st						; Write barier
 */
 
-#if 0
+#if 1
 	// Reconfigure PLLA
 	Register::Write< PllConfig1,
 					 PllConfig1::FracMode,
@@ -99,14 +89,14 @@ void pllInit() {
 					 );
 /*
 ;  ***** PllConfig1::VcoOutPowerDown, is skipped - need to read register ******
-14000ac4:       f57ff04d        dsb     ld				; Read barier
+14000ac4:       f57ff04d        dsb     ld						; Read barier
 14000ac8:       e5931004        ldr     r1, [r3, #4]			; Read R1 <= [0x12010004]
-14000acc:       e2011501        and     r1, r1, #4194304        	; R1 &= 0x00400000, i.e. Clear all bits except of VcoOutPowerDown
-14000ad0:       e301204b        movw    r2, #4171              		; R2 = New value to set bits (Lo bits)
-14000ad4:       e3402300        movt    r2, #768         		; R2 <= 0x0300104B, i.e. set bits 25-DacPowerDown, 24-FracMode-Integer, and values RefDiv=1 & FBDiv=0x4B (75 dec))
-14000ad8:       e1812002        orr     r2, r1, r2			; R1 |= R2 ; Set values 
-14000adc:       e5832004        str     r2, [r3, #4]			; Writr register [0x12010004] <= R2
-14000ae0:       f57ff04e        dsb     st				; Write barier
+14000acc:       e2011501        and     r1, r1, #4194304        ; R1 &= 0x00400000, i.e. Clear all bits except of VcoOutPowerDown
+14000ad0:       e301204b        movw    r2, #4171              	; R2 = New value to set bits (Lo bits)
+14000ad4:       e3402300        movt    r2, #768         		; R2 <= 0x0300104B, i.e. set bits 25-DacPowerDown, 25-FracMode-Integer, and values RefDiv=1 & FBDiv=0x4B (75 dec))
+14000ad8:       e1812002        orr     r2, r1, r2				; R1 |= R2 ; Set values 
+14000adc:       e5832004        str     r2, [r3, #4]
+14000ae0:       f57ff04e        dsb     st
 */
 #else
 
@@ -135,11 +125,58 @@ void pllInit() {
 /*
 ;  ***** PllConfig1::VcoOutPowerDown, is not skipped - don't need to read register before write ******
 14000ac4:       e301204b        movw    r2, #4171       	; R2 = New register value to write bits (Lo bits)
-14000ac8:       e3402300        movt    r2, #768        	; R2 <= 0x0300104B, i.e. set bits bit 25-DacPowerDown, bit 24-FracMode-Integer, and values RefDiv=1 & FBDiv=0x4B (75 dec))
+14000ac8:       e3402300        movt    r2, #768        	; R2 <= 0x0300104B, i.e. set bits 25-DacPowerDown, 25-FracMode-Integer, and values RefDiv=1 & FBDiv=0x4B (75 dec))
 14000acc:       e5832004        str     r2, [r3, #4]		; [0x12010004] <= 0x0300104B, i.e. Write all register bits
-14000ad0:       f57ff04e        dsb     st			; Write barier
+14000ad0:       f57ff04e        dsb     st					; Write barier
 */
 
+#endif
+
+#if 0
+	// So, PllConfig1 and PllConfig7 is the same, but differenet addressed.
+	// How to access another register, but don't describe it?
+	Register::Class<PllConfig1> pllConfig1; // Use default register address (PLLA)
+	Register::Class<PllConfig1> pllConfig7( 0x1201001C );	// Use address of PLLV !!!!!!
+
+	// Reconfigure PLLA
+	pllConfig1.Write<
+					 PllConfig1::FracMode,
+					 PllConfig1::DacPowerDown,
+					 PllConfig1::FoutPowerDown,
+					 PllConfig1::PostdivPowerDown,
+					 PllConfig1::PowerDown,
+					 PllConfig1::Bypass,
+					 PllConfig1::Refdiv,
+					 PllConfig1::FBdiv>(
+						PllConfig1::FracMode::Type::IntegerMode,
+						PllConfig1::DacPowerDown::Type::Normal,
+						PllConfig1::FoutPowerDown::Type::Normal,
+						PllConfig1::PostdivPowerDown::Type::Normal,
+						PllConfig1::PowerDown::Type::Normal,
+						PllConfig1::Bypass::Type::NoBypass,
+						PllConfig1::Refdiv::Type(1),	// 24Mhz / 1 = 24MHz
+						PllConfig1::FBdiv::Type(75)	// 24Mhz * 75 = 1800MHz
+					 );
+
+	// Reconfigure PLLV PLLV, not a PLLA!!!!!
+	pllConfig7.Write<
+					 PllConfig1::FracMode,
+					 PllConfig1::DacPowerDown,
+					 PllConfig1::FoutPowerDown,
+					 PllConfig1::PostdivPowerDown,
+					 PllConfig1::PowerDown,
+					 PllConfig1::Bypass,
+					 PllConfig1::Refdiv,
+					 PllConfig1::FBdiv>(
+						PllConfig1::FracMode::Type::IntegerMode,
+						PllConfig1::DacPowerDown::Type::Normal,
+						PllConfig1::FoutPowerDown::Type::Normal,
+						PllConfig1::PostdivPowerDown::Type::Normal,
+						PllConfig1::PowerDown::Type::Normal,
+						PllConfig1::Bypass::Type::NoBypass,
+						PllConfig1::Refdiv::Type(1),	// 24Mhz / 1 = 24MHz
+						PllConfig1::FBdiv::Type(99)	    // 24Mhz * (99 + 1) = 2400MHz. Frac is in integer mode
+					 );
 #endif
 
 
@@ -155,18 +192,18 @@ void pllInit() {
 	pllWait(100);
 
 	// Wait for PLLA is locked
-	while( PllLockStatus::APll::Type::Locked != PllLockStatus::APll::get() ) {};
+while( PllLockStatus::APll::Type::Locked != PllLockStatus::APll::get() ) {};
 /*
 ; So wait for pll ready
-14000ae8:       e3a02000        mov     r2, #0				; Load base address, low
-14000aec:       e3412201        movt    r2, #4609       		; Load base addres, high. It is PIC-code (compiled with -fPIC)
+14000ae8:       e3a02000        mov     r2, #0			; Load base address, low
+14000aec:       e3412201        movt    r2, #4609       ; Load base addres, high. It is PIC-code (compiled with -fPIC)
 14000af0:       f57ff04d        dsb     ld				; Memory barier
-14000af4:       e59231e8        ldr     r3, [r2, #488]  		; R3 <= [0x120101e8]
-14000af8:       e3130001        tst     r3, #1				; check (R3 & APLL-bit)
+14000af4:       e59231e8        ldr     r3, [r2, #488]  ; R3 <= [0x120101e8]
+14000af8:       e3130001        tst     r3, #1			; check (R3 & APLL-bit)
 14000afc:       0afffffb        beq     14000af0 <_Z7pllInitv+0x6c>	;Loop if not set
 */
 
-#if 0	
+#if 1	
 	while( 
 		!Register::IsEqual<PllLockStatus, 
 						   PllLockStatus::APll,
@@ -176,23 +213,15 @@ void pllInit() {
 /*
 ; So, I can check different flags, or wait on it
 ; This is an example how to wait on both PLLs are locked (APLL & VPLL)
-14000ae8:       e3a02000        mov     r2, #0				; Prepare, load register base address
-14000aec:       e3412201        movt    r2, #4609       		; 0x12010000
+14000ae8:       e3a02000        mov     r2, #0			; Prepare, load register base address
+14000aec:       e3412201        movt    r2, #4609       ; 0x12010000
 14000af0:       f57ff04d        dsb     ld				; Read barer 
-14000af4:       e59231e8        ldr     r3, [r2, #488]  		; R3 <= [0x120101e8], i.e. load reg value
-14000af8:       e1e03003        mvn     r3, r3				; Not R3
-14000afc:       e3130005        tst     r3, #5				; Test, bits 2, 0 is set
-14000b00:       1afffffa        bne     14000af0 <_Z7pllInitv+0x6c> 	;// Loop if not set
-
-****************  The same code with maximum Optimizaion -Os *******************
-14000ad8:       f57ff04d        dsb     ld				; Read barier. r2 preloaded before
-14000adc:       e59231e8        ldr     r3, [r2, #488]  ; 0x1e8		; R3 <= [0x120101e8], i.e. load reg value
-14000ae0:       e2033005        and     r3, r3, #5			; Apply mask
-14000ae4:       e3530005        cmp     r3, #5				; Compare with const (2,0 bits is set = APLL, VPLL)
-14000ae8:       1afffffa        bne     14000ad8 <_Z7pllInitv+0x5c>	; Loop if is not equals
+14000af4:       e59231e8        ldr     r3, [r2, #488]  ; R3 <= [0x120101e8], i.e. load reg value
+14000af8:       e1e03003        mvn     r3, r3			; Not R3
+14000afc:       e3130005        tst     r3, #5			; Test, bits 2, 0 is set
+14000b00:       1afffffa        bne     14000af0 <_Z7pllInitv+0x6c> ;// Loop if not set
 */
 #endif
-
 
 	Register::Write< PllConfig7,
 					 PllConfig7::FracMode,
